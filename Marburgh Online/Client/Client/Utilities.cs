adc3f600c78b2f70231f6ab31f21908197bf4dc2
Marcotte173 <47604041+Marcotte173@users.noreply.github.com>
2020-08-30 03:57:40 -0600

    Add files via upload

---
+++ Marburgh Online/Client/Client/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Utilities
+{
+    internal static void Keypress()
+    {
+        Console.SetCursorPosition(0, 28);
+        Console.WriteLine("Press any key to continue");
+        Console.ReadKey();
+    }
+}
